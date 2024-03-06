775501f194161d285a23d421acc2e3744e6f6e8c
Marco Calderon <marco.calderon1701@hotmail.com>
2024-03-06 12:23:47 -0500

    con fe arreglando

--- src/app/admin/usuarios/components/ModalUsuarios.tsx
+++ src/app/admin/usuarios/components/ModalUsuarios.tsx
@@ -104,7 +104,7 @@ export default function ModalUsuarios({ openModal, setOpenModal, tokenCookie, pe
           <select name="idperfil" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             <option value="">Elija un pefil para el usuario</option>
             {perfiles.map((dato: any) => (
-              <option value={dato.id_perfil}>{dato.nombre_perfil}</option>
+              <option key={dato.id_perfil} value={dato.id_perfil}>{dato.nombre_perfil}</option>
             ))}
           </select>
           {errorMessage2 && (
